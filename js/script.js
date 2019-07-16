6fe70e90fb1aca4b3fe7f5a58e6cda30fd3f67e7
Renan Rodrigo <renanrb@ime.usp.br>
2019-07-16 11:44:00 -0300

    Implementar a logica basica do jogo

--- js/script.js
+++ js/script.js
@@ -2,17 +2,19 @@ function embaralhar(lista) {
   let valor_temporario;
   let indice_aleatorio;
 
-  for (let i = lista.length -1; i !== 0; i--) {
-      indice_aleatorio = Math.floor(Math.random() * i);
+  for (let i = lista.length - 1; i !== 0; i--) {
+    indice_aleatorio = Math.floor(Math.random() * i);
 
-      valor_temporario = lista[i];
-      lista[i] = lista[indice_aleatorio];
-      lista[indice_aleatorio] = valor_temporario;
+    valor_temporario = lista[i];
+    lista[i] = lista[indice_aleatorio];
+    lista[indice_aleatorio] = valor_temporario;
   }
   return lista;
 }
 
 let cartas = document.querySelectorAll(".carta");
+
+let cartaVirada = null;
 
 let imagensSalvas = ["bretta.png", "cornifer.png", "hornet.png", "king.png", "knight.png", "quirrel.png", "vessel.png", "zote.jpg"];
 
@@ -20,17 +22,31 @@ let imagens = imagensSalvas.concat(imagensSalvas);
 
 imagens = embaralhar(imagens);
 
-for(let i = 0; i < cartas.length; i++){
+for (let i = 0; i < cartas.length; i++) {
   cartas[i].style.backgroundImage = `url("imagens/${imagens[i]}")`;
 }
 
-setTimeout(function(){
-  for(let carta of cartas){
+setTimeout(function () {
+  for (let carta of cartas) {
     carta.style.backgroundImage = 'url("imagens/skull.jpg")';
-    carta.onclick = function(){
-      console.log("clicou");
+    carta.onclick = function () {
       carta.style.backgroundImage = `url("imagens/${imagens[Number(carta.id)]}")`;
-      carta.onclick = null;
+      if (cartaVirada && cartaVirada.id !== carta.id) {
+        setTimeout(function () {
+          if (cartaVirada.style.backgroundImage === carta.style.backgroundImage) {
+            cartaVirada.onclick = null;
+            carta.onclick = null;
+          }
+          else {
+            carta.style.backgroundImage = 'url("imagens/skull.jpg")';
+            cartaVirada.style.backgroundImage = 'url("imagens/skull.jpg")';
+          }
+          cartaVirada = null;
+        }, 1500)
+      }
+      else {
+        cartaVirada = carta;
+      }
     }
   }
 }, 3000);
